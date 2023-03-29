5cacfd4d83b8b5bf0c8505754cf46086d0c20d37
Yuriy Khodin <khodiniua@alfastrah.ru>
2023-03-29 13:46:57 +0300

    HW-03 edits

--- hw03-test-framework/src/main/java/ru/otus/annotations/runners/AnnotationRunner.java
+++ hw03-test-framework/src/main/java/ru/otus/annotations/runners/AnnotationRunner.java
@@ -1,6 +1,6 @@
 package ru.otus.annotations.runners;
 
-import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Constructor;
 import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -9,17 +9,16 @@ import java.util.Map;
 import ru.otus.annotations.After;
 import ru.otus.annotations.Before;
 import ru.otus.annotations.Test;
-import ru.otus.test.SmthTest;
 
 public class AnnotationRunner {
 
-    public void startTesting(Class<?> clazz){
+    public void startTesting(Class<?> clazz) {
         Map<String, List<Method>> mappedMethods = collectMethods(clazz);
-        Map<Method, Boolean> results = run(mappedMethods);
+        Map<Method, Boolean> results = run(mappedMethods, clazz);
         showResults(results);
     }
 
-    public Map<String, List<Method>> collectMethods(Class<?> clazz) {
+    private Map<String, List<Method>> collectMethods(Class<?> clazz) {
         Map<String, List<Method>> mappedMethods = new HashMap<>();
         mappedMethods.put("before", new ArrayList<>());
         mappedMethods.put("test", new ArrayList<>());
@@ -30,49 +29,41 @@ public class AnnotationRunner {
             int testCount = method.getAnnotationsByType(Test.class).length;
             int afterCount = method.getAnnotationsByType(After.class).length;
             //collect before methods
-            if (beforeCount == 1) {
+            if (beforeCount > 0) {
                 mappedMethods.get("before").add(method);
-            } else if (testCount > 0 && afterCount > 0) {
-                throw new AnnotationMatchException("Wrong annotations matching for ", method);
             }
             //collect test methods
-            if (testCount == 1) {
+            if (testCount > 0) {
                 mappedMethods.get("test").add(method);
-            } else if (beforeCount > 0 && afterCount > 0) {
-                throw new AnnotationMatchException("Wrong annotations matching for ", method);
             }
             //collect after
-            if (afterCount == 1) {
+            if (afterCount > 0) {
                 mappedMethods.get("after").add(method);
-            } else if (beforeCount > 0 && testCount > 0) {
-                throw new AnnotationMatchException("Wrong annotations matching for ", method);
             }
         }
         return mappedMethods;
     }
 
     private Map<Method, Boolean> run(
-        Map<String, List<Method>> mappedMethods
+        Map<String, List<Method>> mappedMethods,
+        Class<?> clazz
     ) {
         Map<Method, Boolean> results = new HashMap<>();
 
         System.out.println("----------------------------------------------------------------------------------------------");
         for (Method testMethod : mappedMethods.get("test")) {
-            SmthTest smthTest = new SmthTest();
-
+            Object smthTest = null;
+            Constructor<?> constructor;
             try {
+                constructor = clazz.getConstructor();
+                smthTest = constructor.newInstance();
+
                 for (Method beforeMethod : mappedMethods.get("before")) {
                     beforeMethod.invoke(smthTest);
                 }
 
                 testMethod.invoke(smthTest);
                 results.put(testMethod, false);
-            } catch (InvocationTargetException e) {
-                System.out.println("ERROR during test execution, method: " + testMethod);
-                results.put(testMethod, true);
-            } catch (IllegalAccessException e) {
-                System.out.println("ERROR during test execution, method: " + testMethod);
-                results.put(testMethod, true);
             } catch (Exception e) {
                 System.out.println("ERROR during test execution, method: " + testMethod);
                 results.put(testMethod, true);
@@ -81,9 +72,7 @@ public class AnnotationRunner {
                 for (Method afterMethod : mappedMethods.get("after")) {
                     afterMethod.invoke(smthTest);
                 }
-            } catch (InvocationTargetException e) {
-                System.out.println("ERROR during test ending, method: " + testMethod);
-            } catch (IllegalAccessException e) {
+            } catch (Exception e) {
                 System.out.println("ERROR during test ending, method: " + testMethod);
             }
             System.out.println("----------------------------------------------------------------------------------------------");
